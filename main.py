b3b6813ced90ddc962a8470e7d57023c2e61dd4b
Ming-der Wang <mingder78@yahoo.com>
2020-11-20 10:48:19 +0000

    on_error check.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from rx import of
 
 source = of("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
-print(type(source))
-print(dir(source))
-print(source)
-print('----------')
-source.subscribe(lambda value: print("Received {0}".format(value)))
+
+source.subscribe(
+    on_next=lambda i: print("Received {0}".format(i)),
+    on_error=lambda e: print("Error Occurred: {0}".format(e)),
+    on_completed=lambda: print("Done!"),
+)
