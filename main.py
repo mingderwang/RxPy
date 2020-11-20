ad329b562cf0d56a44116b32fb05141212ea36bf
Ming-der Wang <mingder78@yahoo.com>
2020-11-20 10:50:52 +0000

    so simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from rx import of, operators as op
 
-source = of("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
-
-composed = source.pipe(op.map(lambda s: len(s)), op.filter(lambda i: i >= 5))
-composed.subscribe(lambda value: print("Received {0}".format(value)))
+of("Alpha", "Beta", "Gamma", "Delta", "Epsilon").pipe(
+    op.map(lambda s: len(s)),
+    op.filter(lambda i: i >= 5)
+).subscribe(lambda value: print("Received {0}".format(value)))
